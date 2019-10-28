5e104378eaf0cb168d2d7f6ac4c531eb2f4890f8
Victor Bivolaru <bivolaru.victor@gmail.com>
2019-10-28 10:04:11 +0200

    first

---
+++ src/com/tema1/main/Main.java
@@ -0,0 +1,17 @@
+package com.tema1.main;
+
+import com.tema1.goods.Goods;
+
+import java.util.Map;
+
+public final class Main {
+    private Main() {
+        // just to trick checkstyle
+    }
+
+    public static void main(final String[] args) {
+        GameInputLoader gameInputLoader = new GameInputLoader(args[0], args[1]);
+        GameInput gameInput = gameInputLoader.load();
+        //TODO implement homework logic
+    }
+}
